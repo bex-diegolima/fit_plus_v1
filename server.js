dde30d064ec7e0e4fbd8aa65360783880ca0fd02
bex-diegolima <bex.diegolima@gmail.com>
2025-07-25 07:39:55 -0300

    Fix: ajustes gerais no módulo alimentos (frontend + backend) versão GPT3

--- server.js
+++ server.js
@@ -418,13 +418,13 @@ app.post('/api/save-food', authenticateToken, async (req, res) => {
                 vitamina_b12_mcg, vitamina_e_mcg, omega_tres_mg, acido_folico_mcg,
                 teor_alcoolico, observacoes, glutem, categoria_nutricional, origem, nivel_processamento,
                 user_registro, tipo_medida_alimento, dt_registro, dt_atualizacao,
-                status_registro, tipo_registro_alimento,  carga_antioxidante, img_registro
+                status_registro, tipo_registro_alimento,  carga_antioxidante, img_registro, alergicos_comuns
             ) VALUES (
                 $1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
                 $11, $12, $13, $14, $15, $16, $17, $18, $19, $20,
                 $21, $22, $23, $24, $25, $26, $27, $28, $29, $30,
                 $31, $32, $33, $34, $35, $36, $37, $38, NOW(),
-                NOW(), $39, $40, $41, $42
+                NOW(), $39, $40, $41, $42, $43
             )
             RETURNING id
         `;
